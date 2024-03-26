0281a8099dccb9ad0a24a299a684bf094ebdff15
yxz2333 <1467233422@qq.com>
2024-03-26 18:45:08 +0800

    adjust animationtree_dag

--- Character/MARSTON/状态：Attack.gd
+++ Character/MARSTON/状态：Attack.gd
@@ -3,7 +3,7 @@ extends AttackState
 
 func _on_animation_tree_animation_finished(anim_name):
 	if anim_name == attack_1_animation:
-		if timer.is_stopped() or attack_input_cnt <= 1:          # 其实个人感觉这样写攻击切换逻辑不太好，因为这样timer必须在这个(0.2s,0.4s)区间内（攻击动画本身4帧）
+		if timer.is_stopped() or attack_input_cnt <= 1:
 			next_state = return_state
 			playback.travel(move_animation)
 		else:
